e2973137eaf198fa4cfaf109ee957a6c8a9ea592
Fabio-San <fabietto089@gmail.com>
2015-08-15 09:33:28 +0200

    Partial fix heads up improvement
    Change-Id: I08614b7e6e32b3b938af702679fb8df9d5fa5b40

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2105,12 +2105,10 @@ public abstract class BaseStatusBar extends SystemUI implements
         boolean isNoisy = (notification.defaults & Notification.DEFAULT_SOUND) != 0
                 || (notification.defaults & Notification.DEFAULT_VIBRATE) != 0
                 || notification.sound != null
+		|| notification.vibrate != null;
         boolean isHighPriority = sbn.getScore() >= INTERRUPTION_THRESHOLD;
         boolean isFullscreen = notification.fullScreenIntent != null;
         boolean hasTicker = mHeadsUpTicker && !TextUtils.isEmpty(notification.tickerText);
-        int asHeadsUp = notification.extras.getInt(Notification.EXTRA_AS_HEADS_UP,
-                Notification.HEADS_UP_ALLOWED);
-
         boolean isAllowed = asHeadsUp != Notification.HEADS_UP_NEVER;
         boolean accessibilityForcesLaunch = isFullscreen
                 && mAccessibilityManager.isTouchExplorationEnabled();
@@ -2118,6 +2116,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         boolean isClearable = sbn.isClearable(); // not used yet
 
         final KeyguardTouchDelegate keyguard = KeyguardTouchDelegate.getInstance(mContext);
+	boolean interrupt = (isFullscreen || (isHighPriority && (isNoisy || hasTicker))
                 || asHeadsUp == Notification.HEADS_UP_REQUESTED)
                 && isAllowed
                 && !accessibilityForcesLaunch
@@ -2126,9 +2125,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 && !keyguard.isInputRestricted()
                 && !zenBlocksHeadsUp
                 && !isImeShowing();
-                && (!mStatusBarKeyguardViewManager.isShowing()
-                        || mStatusBarKeyguardViewManager.isOccluded())
-                && !mStatusBarKeyguardViewManager.isInputRestricted();
         try {
             interrupt = interrupt && !mDreamManager.isDreaming();
         } catch (RemoteException e) {
